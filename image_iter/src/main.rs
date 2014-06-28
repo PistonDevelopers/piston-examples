b3b3784d7e802e2c1e0efbd9fd31f062a742a9eb
Sven Nilsen <bvssvni@gmail.com>
2014-06-28 12:39:53 +0200

    Using for loop in 'image_iter' example

--- image_iter/src/main.rs
+++ image_iter/src/main.rs
@@ -44,21 +44,17 @@ fn main() {
             updates_per_second: 120,
             max_frames_per_second: 60,
         };
-    let mut game_iter = GameIterator::new(&mut window, &game_iter_settings);
     let ref mut gl = Gl::new();
-    loop {
-        match game_iter.next() {
-            None => { break },
-            Some(e) => match e {
-                Render(args) => {
-                    gl.viewport(0, 0, args.width as i32, args.height as i32);
+    for e in GameIterator::new(&mut window, &game_iter_settings) {
+        match e {
+            Render(args) => {
+                gl.viewport(0, 0, args.width as i32, args.height as i32);
 
-                    let c = Context::abs(args.width as f64, args.height as f64);
-                    c.rgb(1.0, 1.0, 1.0).draw(gl);
+                let c = Context::abs(args.width as f64, args.height as f64);
+                c.rgb(1.0, 1.0, 1.0).draw(gl);
                     c.image(&image).draw(gl);
-                },
-                _ => {},       
             },
+            _ => {},
         }
     }
 }
