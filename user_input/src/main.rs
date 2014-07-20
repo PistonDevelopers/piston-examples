0b1767552958916e8e989abcf6602c2d825196ab
Sven Nilsen <bvssvni@gmail.com>
2014-07-20 22:38:21 +0200

    Added capture of cursor
    Closes https://github.com/PistonDevelopers/piston-examples/issues/11

--- user_input/src/main.rs
+++ user_input/src/main.rs
@@ -8,30 +8,46 @@ extern crate glfw_game_window;
 // use Window = sdl2_game_window::GameWindowSDL2;
 use Window = glfw_game_window::GameWindowGLFW;
 use piston::{
+    keyboard,
     Game,
     GameIteratorSettings,
     GameWindowSettings,
     KeyPressArgs,
     KeyReleaseArgs,
+    MouseMoveArgs,
+    MouseRelativeMoveArgs,
     MousePressArgs,
     MouseReleaseArgs,
     MouseScrollArgs,
 };
 
-pub struct App;
+pub struct App {
+    capture_cursor: bool
+}
 
 impl App {
     /// Creates a new application.
     pub fn new() -> App {
-        App
+        App { 
+            capture_cursor: false 
+        }
     }
 }
 
 impl Game for App {
+    fn should_capture_cursor(&mut self) -> bool {
+        self.capture_cursor
+    }
+
     fn key_press(
         &mut self,
         args: &KeyPressArgs
     ) {
+        if args.key == keyboard::C {
+            println!("Turned capture cursor on");
+            self.capture_cursor = !self.capture_cursor;
+        }
+
         println!("Pressed keyboard key '{}'", args.key);
     }
 
@@ -40,6 +56,20 @@ impl Game for App {
         args: &KeyReleaseArgs
     ) {
         println!("Released keyboard key '{}'", args.key);
+    }
+
+    fn mouse_move(
+        &mut self,
+        args: &MouseMoveArgs
+    ) {
+        println!("Mouse moved '{} {}'", args.x, args.y);
+    }
+
+    fn mouse_relative_move(
+        &mut self,
+        args: &MouseRelativeMoveArgs
+    ) {
+        println!("Relative mouse moved '{} {}'", args.dx, args.dy);
     }
 
     fn mouse_press(
@@ -73,6 +103,8 @@ fn main() {
             exit_on_esc: true,
         }
     );
+
+    println!("Press C to turn capture cursor on/off");
 
     let mut app = App::new();
     let game_iter_settings = GameIteratorSettings {
