328d15ac535cdc7c760d21d4de60f7c9917d44b6
Sven Nilsen <bvssvni@gmail.com>
2014-09-25 15:34:00 +0200

    Updated user_input example to use generic events
    * Added git ignore filter for sprite

--- user_input/src/main.rs
+++ user_input/src/main.rs
@@ -1,59 +1,25 @@
 #![feature(globs)]
 
-extern crate graphics;
 extern crate piston;
 // extern crate sdl2_game_window;
 extern crate glfw_game_window;
 
 // use sdl2_game_window::WindowSDL2;
 use glfw_game_window::WindowGLFW;
-use piston::input::keyboard;
-use piston::input::{
-    Focus,
-    Keyboard,
-    Mouse,
-    MouseCursor,
-    MouseRelative,
-    MouseScroll,
-    Move,
-    Press,
-    Release,
-    Resize,
-    Text,
+use piston::Window;
+use piston::input::{ keyboard, Keyboard, Mouse };
+use piston::event::{
+    FocusEvent,
+    PressEvent,
+    MouseCursorEvent,
+    MouseRelativeEvent,
+    MouseScrollEvent,
+    ReleaseEvent,
+    RenderEvent,
+    ResizeEvent,
+    TextEvent,
+    UpdateEvent,
 };
-use piston::{
-    Input,
-    Render,
-    Update
-};
-
-pub struct App {
-    capture_cursor: bool
-}
-
-impl App {
-    /// Creates a new application.
-    pub fn new() -> App {
-        App { 
-            capture_cursor: false 
-        }
-    }
-
-    fn key_press<W: piston::Window>(
-        &mut self,
-        window: &mut W,
-        key: keyboard::Key
-    ) {
-        if key == keyboard::C {
-            println!("Turned capture cursor on");
-            self.capture_cursor = !self.capture_cursor;
-
-            window.capture_cursor(self.capture_cursor);
-        }
-
-        println!("Pressed keyboard key '{}'", key);
-    }
-}
 
 fn main() {
     let mut window = WindowGLFW::new(
@@ -69,7 +35,7 @@ fn main() {
 
     println!("Press C to turn capture cursor on/off");
 
-    let mut app = App::new();
+    let mut capture_cursor = false;
     let event_settings = piston::EventSettings {
             updates_per_second: 120,
             max_frames_per_second: 60,
@@ -81,28 +47,37 @@ fn main() {
                 Some(e) => e
             };
 
-        match e {
-            Input(Press(Keyboard(key))) => 
-                app.key_press(event_iter.window, key),
-            Input(Release(Keyboard(key))) => 
-                println!("Released keyboard key '{}'", key),
-            Input(Press(Mouse(button))) => 
-                println!("Pressed mouse button '{}'", button),
-            Input(Release(Mouse(button))) => 
-                println!("Released mouse button '{}'", button),
-            Input(Move(MouseCursor(x, y))) => 
-                println!("Mouse moved '{} {}'", x, y),
-            Input(Move(MouseScroll(dx, dy))) => 
-                println!("Scrolled mouse '{}, {}'", dx, dy),
-            Input(Move(MouseRelative(dx, dy))) => 
-                println!("Relative mouse moved '{} {}'", dx, dy),
-            Input(Text(text)) => println!("Typed '{}'", text),
-            Input(Resize(w, h)) => println!("Resized '{}, {}'", w, h),
-            Input(Focus(true)) => println!("Gained focus"),
-            Input(Focus(false)) => println!("Lost focus"),
-            Render(_) => {},
-            Update(_) => {},
-        }
+        e.press(|button| {
+            match button {
+                Keyboard(key) => {
+                    if key == keyboard::C {
+                        println!("Turned capture cursor on");
+                        capture_cursor = !capture_cursor;
+                        event_iter.window.capture_cursor(capture_cursor);
+                    }
+
+                    println!("Pressed keyboard key '{}'", key);
+                }, 
+                Mouse(button) => println!("Pressed mouse button '{}'", button),
+            }
+        });
+        e.release(|button| {
+            match button {
+                Keyboard(key) => println!("Released keyboard key '{}'", key),
+                Mouse(button) => println!("Released mouse button '{}'", button),
+            }
+        });
+        e.mouse_cursor(|x, y| println!("Mouse moved '{} {}'", x, y));
+        e.mouse_scroll(|dx, dy| println!("Scrolled mouse '{}, {}'", dx, dy));
+        e.mouse_relative(|dx, dy| println!("Relative mouse moved '{} {}'", dx, dy));
+        e.text(|text| println!("Typed '{}'", text));
+        e.resize(|w, h| println!("Resized '{}, {}'", w, h));
+        e.focus(|focused| {
+            if focused { println!("Gained focus"); }
+            else { println!("Lost focus"); }
+        });
+        e.render(|_| {});
+        e.update(|_| {});
     }
 }
 
