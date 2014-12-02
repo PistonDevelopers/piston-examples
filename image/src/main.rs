4d00f2ba1e725e02fd338ff6adc0665ff104f22c
Sven Nilsen <bvssvni@gmail.com>
2014-12-02 10:47:20 +0100

    Use image method

--- image/src/main.rs
+++ image/src/main.rs
@@ -38,7 +38,7 @@ fn main() {
             use graphics::*;
             gl.draw([0, 0, args.width as i32, args.height as i32], |c, gl| {
                 graphics::clear([1.0, ..4], gl);
-                graphics::Image::new().draw(&image, &c, gl);
+                graphics::image(&image, &c, gl);
             });
         });
     }
