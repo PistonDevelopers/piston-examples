2b0c366d907263a73482c9430e6ee446e6a135cf
Nicholas Wright <nick.loves.rails@gmail.com>
2015-09-12 08:00:24 -0600

    connect the dots in paint example app to smooth out line

--- src/paint.rs
+++ src/paint.rs
@@ -1,8 +1,10 @@
 extern crate piston_window;
 extern crate image as im;
+extern crate vecmath;
 
 use im::GenericImage;
 use piston_window::*;
+use vecmath::*;
 
 fn main() {
     let opengl = OpenGL::V3_2;
@@ -21,6 +23,9 @@ fn main() {
             &canvas,
             &TextureSettings::new()
         ).unwrap();
+
+    let mut last_pos = None;
+
     for e in window {
         e.draw_2d(|c, g| {
             clear([1.0; 4], g);
@@ -28,22 +33,43 @@ fn main() {
         });
         if let Some(button) = e.press_args() {
             if button == Button::Mouse(MouseButton::Left) {
-                draw = true
+                draw = true;
+                last_pos = e.mouse_cursor_args()
             }
         };
         if let Some(button) = e.release_args() {
             if button == Button::Mouse(MouseButton::Left) {
-                draw = false
+                draw = false;
+                last_pos = None
             }
         };
         if draw {
             if let Some(pos) = e.mouse_cursor_args() {
-                let (x, y) = (pos[0] as u32, pos[1] as u32);
-                if x < width && y < height {
-                    canvas.put_pixel(x, y, im::Rgba([0, 0, 0, 255]));
-                    texture.update(&mut*e.factory.borrow_mut(), &canvas).unwrap();
-                }
+                let (x, y) = (pos[0] as f32, pos[1] as f32);
+
+                match last_pos {
+                    Some(p) => {
+                        let (last_x, last_y) = (p[0] as f32, p[1] as f32);
+                        let distance = vec2_len(vec2_sub(p, pos)) as u32;
+
+                        for i in 0..distance {
+                            let diff_x = (x - last_x) as f32;
+                            let diff_y = (y - last_y) as f32;
+                            let delta = i as f32 / distance as f32;
+                            let new_x = (last_x + (diff_x * delta)) as u32;
+                            let new_y = (last_y + (diff_y * delta)) as u32;
+                            if new_x < width && new_y < height {
+                                canvas.put_pixel(new_x, new_y, im::Rgba([0, 0, 0, 255]));
+                                texture.update(&mut*e.factory.borrow_mut(), &canvas).unwrap();
+                            };
+                        };
+                    },
+                    None => {}
+                };
+
+                last_pos = Some(pos)
             };
+
         }
     }
 }
